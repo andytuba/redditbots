90f862173f63c0c32d78c8f2bb2c0e452d6ad159
andytuba <andytuba@andytuba.com>
2016-03-01 21:35:44 -0800

    move virtualenv setup to other repo, simplify bot filename handler

--- setup.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d bin ]; then
-	virtualenv .
-	source bin/activate
-	pip install -r requirements.txt
-fi
--- updatesubreddit/stylesheets.py
+++ updatesubreddit/stylesheets.py
@@ -16,63 +16,53 @@ class SubredditStylesheetUpdater(RedditBot):
 		'example': 'body { background: white; }'
 	}
 
+	def __init__(self, filenames):
+		super(self.__class__, self).__init__()
+		if isinstance(filenames, basestring):
+			self.filenames = [ filenames ]
+		else:
+			self.filenames = filenames
+
 	def main(self, r):
-		glob = filename_param()
+		for filename in self.filenames:
+			self.push(filename)
+
+	def push(self, filename):
 		#self.log_debug
-		print('File glob: %s' % glob)
-		for filename in files(glob):
-			#self.log_debug
-			print('Handling %s' % filename)
-			subreddit_name = os.path.splitext(os.path.basename(filename))[0]
-			contents = file_get_contents(filename)
+		print('Handling %s' % filename)
+		subreddit_name = os.path.splitext(os.path.basename(filename))[0]
+		contents = file_get_contents(filename)
 
-			#self.log_debug
-			print("/r/%s will be updated with: \n%s" % (subreddit_name, contents))
+		#self.log_debug
+		print("/r/%s will be updated with: \n%s" % (subreddit_name, contents))
 
-			if self.stylesheets.get(subreddit_name) != contents:
-				#self.log_info
-				print('/r/%s/about/stylesheet is updating...' % (subreddit_name))
-				r.set_stylesheet(subreddit_name, contents)
-				self.stylesheets[subreddit_name] = contents
-
+		if self.stylesheets.get(subreddit_name) != contents:
 			#self.log_info
-			print('/r/%s/about/stylesheet is up to date' % (subreddit_name))
+			print('/r/%s/about/stylesheet is updating...' % (subreddit_name))
+			r.set_stylesheet(subreddit_name, contents)
+			self.stylesheets[subreddit_name] = contents
+
+		#self.log_info
+		print('/r/%s/about/stylesheet is up to date' % (subreddit_name))
 
 
 	# just for debugging
 	#def run(self): self.main(r=None)
 
 
-def filename_param():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("src_path", metavar="path", type=str,
-	    help="Path to files to be merged; enclose in quotes, accepts * as wildcard for directories or filenames")
-
-	args = parser.parse_args()
-	return args.src_path
-
-def files(glob_value):
-	files = glob.iglob(glob_value)
-	#Should I just return files here?
-	try:
-	    first_file = files.next()
-	except StopIteration:
-	    print('File does not exist: %s' % glob_value)
-	    sys.exit(1)
-
-	return itertools.chain([first_file], files)
-
 def file_get_contents(filename):
 	    with open(filename) as f:
 	        return f.read()
 
 
-
-
-
-
-
 if __name__ == '__main__':
-    bot = SubredditStylesheetUpdater()
-    bot.run()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("src_path", metavar="path", type=str,
+	    help="Path to file")
+
+	args = parser.parse_args()
+	filename = args.src_path
+
+	bot = SubredditStylesheetUpdater(filename)
+	bot.run()
 
